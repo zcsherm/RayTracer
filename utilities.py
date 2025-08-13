578e726d6dfd81b683868b564f4ca9482ca37907
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-08-13 16:28:31 -0700

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+IDENTITY = np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+])
+
+YAW = lambda yaw: np.array([
+    [np.cos(yaw), 0, np.sin(yaw)],
+    [0, 1, 0],
+    [-np.sin(yaw), 0, np.cos(yaw)]
+])
+
+PITCH = lambda pitch: np.array([
+    [1, 0, 0],
+    [0, np.cos(pitch), -np.sin(pitch)],
+    [0, np.sin(pitch), np.cos(pitch)]
+])
+
+ROLL = lambda roll: np.array([
+    [np.cos(roll), -np.sin(roll), 0],
+    [np.sin(roll), np.cos(roll), 0],
+    [0, 0, 1]
+])
+
+def rotate(yaw, pitch, roll, vector):
+    R = np.dot(YAW(yaw), (PITCH(pitch), ROLL(roll)))
+    return np.dot(R, vector
